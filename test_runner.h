1a9d45c887ca8753a93ab6aba6e0df58f6367821
ZhekLu <justletmego@list.ru>
2021-09-12 17:21:01 +0300

    logger task

--- test_runner.h
+++ test_runner.h
@@ -94,3 +94,20 @@ public:
 private:
   int fail_count = 0;
 };
+
+#define ASSERT_EQUAL(x, y) {            \
+  ostringstream os;                     \
+  os << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;     \
+  AssertEqual(x, y, os.str());          \
+}
+
+#define ASSERT(x) {                     \
+  ostringstream os;                     \
+  os << #x << " is false, "             \
+    << __FILE__ << ":" << __LINE__;     \
+  Assert(x, os.str());                  \
+}
+
+#define RUN_TEST(tr, func) \
+  tr.RunTest(func, #func)
